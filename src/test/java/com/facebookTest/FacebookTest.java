eba194ec849b18a731219dc2d4f8726d03b20d4c
Mohammed Molla <mjmolla@gmail.com>
2023-11-11 15:54:22 -0500

    open application

--- src/test/java/com/facebookTest/FacebookTest.java
+++ src/test/java/com/facebookTest/FacebookTest.java
@@ -1,12 +1,33 @@
 package com.facebookTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 
 public class FacebookTest {
 
 	
 	@Test
 	public void titleTest() {
+		
+		
+		//open browser
+		//  <groupId>io.github.bonigarcia</groupId>
+	    //<artifactId>webdrivermanager</artifactId>
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver= new ChromeDriver();
+				//Parent c= new Child();
+				
+	
+		
+		
+		//open application
+		//c.house();
+		driver.get("https://www.facebook.com/");
+		
 	
 		System.out.println("Hello Selenium");
 		
